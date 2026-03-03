a306ff2e22977b586a8bcbd1a5e382601a149e58
Eimilly__S2 <emillysantosaaraujo@gmail.com>
2026-03-03 18:44:28 -0300

    Add files via upload
    10.              Receba 2 números reais. Calcule e mostre a diferença desses valores.

---
+++ Questão10.py
@@ -0,0 +1,14 @@
+#Declarar
+
+R1 : int = 0
+R2 : int = 0
+Diferença : int = 0
+
+#Inicio
+
+R1 = int (input("Digite o primeiro número: "))
+R2 = int (input("Digite o segundo número: "))
+Diferença = (R1 - R2)
+print ("A diferença dos números é: ",Diferença)
+
+#Fim
